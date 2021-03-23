eb29edf37ad6901b1950080756bd3c944c04fb26
Sandeep Bochaliya <sandeepbochaliya@gmail.com>
2021-03-23 23:03:13 +0530

    backend part is done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Post, Body} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {
+        
+    }
     @Post()
-    create(@Body() createUserDto: CreateUserDto): string {
-        return `UserName: ${createUserDto.userName} Score: ${createUserDto.score}`;
+    create(@Body() createUserDto: CreateUserDto): Promise<any[]> {
+        return this.usersService.addUser(createUserDto.userName, createUserDto.score);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,10 +4,18 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private readonly userModel){
+    constructor(@InjectModel('User') private readonly userModel) {
 
     }
+
+    async addUser(userName, score): Promise<any[]> {
+        let scoreDate = new Date();
+        scoreDate.setHours(0, 0, 0, 0);
+        scoreDate = new Date();
+        return this.userModel.insertMany([{ user: userName, score: score, score_date: scoreDate }])
+    }
+
     async getTopScores(): Promise<any[]> {
-        return this.userModel.find().sort({score: -1}).limit(10);
+        return this.userModel.find().sort({ score: -1 }).limit(10);
     }
 }
