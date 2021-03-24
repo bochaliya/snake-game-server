ad97cf96ea2f8c546c336f120d4e38607ff0401a
Sandeep Bochaliya <sandeepbochaliya@gmail.com>
2021-03-24 13:26:37 +0530

    remaining top score functions added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -18,4 +18,42 @@ export class UsersService {
     async getTopScores(): Promise<any[]> {
         return this.userModel.find().sort({ score: -1 }).limit(10);
     }
+
+    async getTopScoresPerUser(): Promise<any[]> {
+        const pipeline = [
+            { $sort: { user: -1, score: -1 } },
+            {
+                $group:
+                    { '_id': '$user', 'scores': { '$push': '$$ROOT' } }
+            },
+            {
+                $project:
+                    { 'topTen': { '$slice': ['$scores', 10] } }
+            }
+        ]
+        return this.userModel.aggregate(pipeline);
+    }
+
+    async totalGames(): Promise<any[]> {
+        const pipeline = [
+            {
+                $group: {
+                    _id: '$score_date', count: { $sum: 1 }
+                }
+            }
+        ]
+        return this.userModel.aggregate(pipeline);
+    }
+
+    async totalGamesPerUser(): Promise<any[]> {
+        const pipeline = [
+            {
+                $group: {
+                    _id: { score_date: '$score_date', user: '$user' },
+                    count: { $sum: 1 }
+                }
+            }
+        ]
+        return this.userModel.aggregate(pipeline);
+    }
 }
